769841f46583acfe6041a55c79e5373e09c8c442
Oliver Steele <steele@osteele.com>
2018-01-24 11:44:28 -0500

    Web server uses a logger; message is optional

--- twilio_gateway.py
+++ twilio_gateway.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import sys
 
@@ -8,6 +9,13 @@ import sys
 sys.path.append('./scripts')
 from send_mqtt_messages import publish
 
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger('messages')
+
+HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
+PORT = int(os.environ.get('PORT', 5000))
+DEBUG = 'PORT' not in os.environ
+RESPONSE_TEXT = os.environ.get('RESPONSE_TEXT')
 
 app = Flask(__name__)
 
@@ -16,15 +24,14 @@ app = Flask(__name__)
 def sms_webhook():
     topic = request.form['To'].replace('+', 'incoming-sms-')
     payload = dict(request.form)
-    print('publish', payload, 'to', topic)
+    logger.info('publish {} to {}'.format(payload, topic))
     publish(topic, **request.form)
 
     resp = MessagingResponse()
-    msg = resp.message("Your message has been relayed.")
+    if RESPONSE_TEXT:
+        msg = resp.message(RESPONSE_TEXT)
     return str(resp)
 
-HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
-DEBUG = 'PORT' not in os.environ
-PORT = int(os.environ.get('PORT', 5000))
-print('Listening on', PORT)
+logger.setLevel(logging.INFO)
+logger.info('Listening on {}'.format(PORT))
 app.run(host=HOST, port=PORT, debug=DEBUG)
