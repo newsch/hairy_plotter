a6b3e88ccd9a4dfa44004bf32e7ee2ade970eebf
Oliver Steele <steele@osteele.com>
2018-01-25 09:26:02 -0500

    TTS uses `say` on macOS

--- scripts/tts_worker.py
+++ scripts/tts_worker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import logging
-import os
+import platform
+import subprocess
 
 import click
 from receive_mqtt_messages import create_subscription_queue
@@ -10,13 +11,19 @@ logger = logging.getLogger('speaker')
 logger.setLevel(logging.INFO)
 
 
+SPEECH_COMMAND = 'say' if platform.system() == 'Darwin' else 'espeak'
+
+
 @click.command()
 @click.option('--topic', default='speak')
 def main(topic):
     for msg in create_subscription_queue(topic):
-        message = msg['message']
-        logger.info(msg)
-        os.system("espeak '{}'".format(message))
+    message = msg['message']
+    logger.info(msg)
+    res = subprocess.run([SPEECH_COMMAND, message],
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if res.returncode != 0:
+        logger.error(res.stderr.decode().strip())
 
 if __name__ == '__main__':
     main()
