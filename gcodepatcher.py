57613862fd326bfd1a6af8763de184eba870ab32
Evan New-Schmidt <evan@new-schmidt.com>
2018-10-31 00:39:54 -0400

    Refactor gcodepatcher

--- gcodepatcher.py
+++ gcodepatcher.py
@@ -1,53 +1,135 @@
 #!/usr/bin/env python3
 import argparse
+import logging
 import re
 
-##### VARIABLES #####
-Z_Retract_Height    = "G0 Z2"       # this is the upper height/ where Z goes before rapid traversing (pen shouldn't be touching)
-Z_Cut_Height        = "G1 Z-1"      # this is the lower height/ where Z goes during cutting (pen down)
+
+logger = logging.getLogger(__name__)
+
+
+FEED_RATE = 9000
+PEN_PAUSE = 0.75  # time in seconds to pause when pen is moving up.down
 
 # servo positions
 PEN_UP = 525
 PEN_1_DOWN = 950
 PEN_2_DOWN = 1
 
-# template for pen movement
-PEN_MOVE_TEMP = "M03 S{}"
-PAUSE_TEMP = "G4 P{}"
 
-SEND_HOME = "G0X0Y0"
+HOME_PATTERN = "G0X0Y0"
 
-Z_LOWER = 0
-Z_UPPER = 1
+MOVE_TEMP = "G0 X{} Y{}"
+
+PEN_MOVE_TEMP = "M03 S{}"  # template for pen movement
+PAUSE_TEMP = "G4 P{}"  # template for pausing (in seconds)
+
+FEED_TEMP = "F{}"
+
+def pause(time):
+    return PAUSE_TEMP.format(time)
+
+def pen1down():
+    return PEN_MOVE_TEMP.format(PEN_1_DOWN)
+
+def pen2down():
+    return PEN_MOVE_TEMP.format(PEN_2_DOWN)
+
+def penup():
+    return PEN_MOVE_TEMP.format(PEN_UP)
+
+def movehome():
+    return MOVE_TEMP.format(0, 0)
+
+def setfeed(rate):
+    return FEED_TEMP.format(rate)
+
+
+SPEED_PATTERN = "(-?\d+.?\d*)"  # general regex for getting commands
+
+# match on G0/G1 commands with Z settings
+# https://regexr.com/4222s
 Z_PATTERN = "(G[01]) ?([\w\d.]*)Z(-?\d+.?\d*)([\w\d.]*)"
 Z_COMPILED = re.compile(Z_PATTERN)
+Z_LOWER = 0
+Z_UPPER = 1
 
-skip_list = [
-    "M6"
+# commands to remove inline
+STRIP_LIST = [
+    re.compile('F'+SPEED_PATTERN),  # feed commands
+    re.compile('P\d')  # pause commands
+]
+
+# commands to skip line when reading
+SKIP_LIST = [
+    "M6",  # tool change
+    movehome(),  # 0,0 return
 ]
 
 
-def z_replace(matchobj):
-    # TODO: remove F commands
-    cmds = []
-    code = matchobj.group(1)
-    pre = matchobj.group(2)
-    zmove = float(matchobj.group(3))
-    post = matchobj.group(4)
-    if pre:
-        cmds.append(code+pre)
-    if zmove <= Z_LOWER:
-        cmds.append(PEN_MOVE_TEMP.format(PEN_1_DOWN))
-        cmds.append(PAUSE_TEMP.format(0.5))
-    elif zmove >= Z_UPPER:
-        cmds.append(PEN_MOVE_TEMP.format(PEN_UP))
-        cmds.append(PAUSE_TEMP.format(0.5))
+def skip_commands(line):
+    """Skip lines if they contain specified commands."""
+    for cmd in SKIP_LIST:
+        if cmd in line:
+            logger.debug(
+                "Skipped line {!r} from pattern {!r}".format(line,cmd))
+            return []
+    return [line]
 
-    if post:
-        cmds.append(code+post)
-    return '\n'.join(cmds)
+def strip_commands(line):
+    """Remove commands inline using regex."""
+    for reg in STRIP_LIST:
+        if reg.search(line) is not None:
+            logger.debug('Stripped line {!r} w/ pattern {!r}'.format(
+                line, reg.pattern))
+            line = reg.sub('', line)  # "substitute" w/ empty string
+    if line.strip() == '':
+        logger.debug('Removed empty stripped line')
+        return []  # return empty if string has no more commands
+    else:
+        return [line]
+
+def patch_z(line):
+    """Replace Z movements with custom tool movements."""
+    def z_replace(matchobj):
+        """Replace function for match object."""
+        cmds = []
+        code = matchobj.group(1)
+        pre = matchobj.group(2)
+        zmove = float(matchobj.group(3))
+        post = matchobj.group(4)
+        if pre:
+            cmds.append(code+pre)
+        if zmove <= Z_LOWER:
+            cmds.append(PEN_MOVE_TEMP.format(PEN_1_DOWN))
+            cmds.append(PAUSE_TEMP.format(PEN_PAUSE))
+        elif zmove >= Z_UPPER:
+            cmds.append(PEN_MOVE_TEMP.format(PEN_UP))
+            cmds.append(PAUSE_TEMP.format(PEN_PAUSE))
+        if post:
+            cmds.append(code+post)
+        return '\n'.join(cmds)
+
+    if Z_COMPILED.match(line):
+        new_lines = []
+        cmds = Z_COMPILED.sub(z_replace, line)
+        logger.debug('Replaced z command from line {!r} with {!r}'.format(
+            line, cmds))
+        for cmd in cmds.splitlines():
+            new_lines.append(cmd)
+        return new_lines
+    else:
+        return [line]
+
+PROCESS_ORDER = [  # functions to use and the order to use them in
+    skip_commands,
+    strip_commands,
+    patch_z
+]
+
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('infile', type=argparse.FileType('r'),
@@ -57,40 +139,31 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # modify gcode
-    new_lines = []  # where the new modified code will be put
-    new_lines.append('G0 F1500')  # set feed rate
-
     content = args.infile.read().splitlines()  # gcode as a list where each element is a line
+    new_lines = []  # where the new modified code will be put
 
     for line in content:
-        # replace Z codes w/ pen change
-        # import pdb; pdb.set_trace()
-        skip = False
-        for cmd in skip_list:
-            if cmd in line:
-                skip = True
-        if skip:
-            continue  # skip to next line
+        # run through stateless single-line processors
+        inputs = [line]
+        for step in PROCESS_ORDER:
+            for l in inputs:
+                outputs = [result for result in step(l)]
+            inputs = outputs
+        [new_lines.append(output) for output in outputs]
 
-        if Z_COMPILED.match(line):
-            cmds = Z_COMPILED.sub(z_replace, line)
-            for cmd in cmds.splitlines():
-                new_lines.append(cmd)
-        else:
-            new_lines.append(line)
+    HEADER = [  # commands to add at the beginning of the file
+        setfeed(FEED_RATE)
+    ]
 
-        # ##### INSERT PEN COMMANDS - REPLACE Z TRANSLATION WITH SERVO ROTATION #####
-        # #if line contains z retract text, replace with pen up
-        # if Z_Retract_Height in line:
-        #     new_code += line.replace(Z_Retract_Height, PEN_UP)
-        # #if line contains z cut text, replace with pen down
-        # elif Z_Cut_Height in line:
-        #     new_code += line.replace(Z_Cut_Height, PEN_DOWN)
-        # else:
-        #     new_code += line
+    FOOTER = [  # commands to add at the end of the file
+        penup(),
+        pause(PEN_PAUSE)
+    ]
 
-    new_lines.append(PEN_MOVE_TEMP.format(PEN_1_DOWN))
-    new_lines.append(PEN_MOVE_TEMP.format(PEN_UP))
-    new_lines.append(SEND_HOME)
-    # output new_code
+    new_lines = [
+        *HEADER,
+        *new_lines,
+        *FOOTER
+    ]
+
     args.outfile.write('\n'.join(new_lines)+'\n')
