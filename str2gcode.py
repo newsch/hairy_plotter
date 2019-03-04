2185bbb54207844dacaf894fc3e8094672820331
Evan New-Schmidt <evan@new-schmidt.com>
2019-03-03 21:53:32 -0500

    Separate horizontal and vertical margins

--- str2gcode.py
+++ str2gcode.py
@@ -17,7 +17,8 @@ FONT_BASE_HEIGHT = 21  # in mm
 SHEET_WIDTH = 750  # in mm
 # SHEET_HEIGHT  # roller has no fixed height
 LINE_SPACING = 0.5  # in characters
-MARGIN = 25
+MARGIN_WIDTH = 0
+MARGIN_HEIGHT = 0
 
 MAX_HEIGHT = 800  # max height of text in document units
 
@@ -37,7 +38,7 @@ def calculate_parameters():
     global MAX_LINES
     global wrapper
 
-    MAX_WIDTH = SHEET_WIDTH - MARGIN*2  # max width of text in document units
+    MAX_WIDTH = SHEET_WIDTH - MARGIN_WIDTH*2  # max width of text in document units
     CHAR_WIDTH = FONT_BASE_WIDTH*FONT_SCALE  # in document units
     CHAR_HEIGHT = FONT_BASE_HEIGHT*FONT_SCALE  # in document units
 
@@ -69,20 +70,20 @@ def lines_to_gcode(lines: list, align='left'):
     # calculate sheet dimensions
     # width = max([len(l) for l in lines])*CHAR_WIDTH + 2*MARGIN
     width = SHEET_WIDTH
-    height = (1 + (len(lines) - 1)*(1 + LINE_SPACING))*CHAR_HEIGHT + 2*MARGIN
+    height = (1 + (len(lines) - 1)*(1 + LINE_SPACING))*CHAR_HEIGHT + 2*MARGIN_HEIGHT
 
     lines.reverse()  # flip lines for printing (gcode origin is bottom left)
     gcodes = []
     for i, line in enumerate(lines):
         # horizontal alignment
         if align == 'left':
-            x = MARGIN
+            x = MARGIN_WIDTH
         elif align == 'right':
-            x = SHEET_WIDTH - MARGIN - len(line)*CHAR_WIDTH
+            x = SHEET_WIDTH - MARGIN_WIDTH - len(line)*CHAR_WIDTH
         elif align == 'center':
-            x = MARGIN + (SHEET_WIDTH - 2*MARGIN - len(line)*CHAR_WIDTH)/2
+            x = MARGIN_WIDTH + (SHEET_WIDTH - 2*MARGIN_WIDTH - len(line)*CHAR_WIDTH)/2
 
-        y = MARGIN + (i*LINE_SPACING + i)*CHAR_HEIGHT
+        y = MARGIN_HEIGHT + (i*LINE_SPACING + i)*CHAR_HEIGHT
         # y = 400/2 + (i*LINE_SPACING + (i+1))*CHAR_HEIGHT  # for testing
         gcodes.append(gcodefont_wrapper(line, x, y, FONT_SCALE))
 
