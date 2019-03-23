0ca0d9d7bd278d8d21d3287600d75c5f38d2daac
Evan New-Schmidt <evan@new-schmidt.com>
2019-03-23 19:22:01 -0400

    Get working hershey parser

--- font.py
+++ font.py
@@ -3,73 +3,46 @@ import gcode as g
 
 import matplotlib.pyplot as plt
 
-# _HERSHEY = {
-#     'a':
-# }
-
-
-
-def parse_hershey_char(code):
-    print(code)
-    ctoi = lambda a: ord(a) - ord('R')
-    get_coord = lambda a: (tuple(map(ctoi, a[i:i+2])) for i in range(0, len(a), 2))
-    # assert(code[0]==' ')
-    # assert(code[-1] == '\n')
-    char_num = int(code[0:4].strip())
-    num_vertices = int(code[4:7].strip())
-    left = ctoi(code[7])
-    right = ctoi(code[8])
-
-    path_strs = code[9:].replace('\n', '').split(' R')
-    paths = tuple(tuple(get_coord(p)) for p in path_strs)
-
-    counted_vertices = (len(paths)-1) + sum((len(p) for p in paths)) + 1
-
-
-    # print(char_num, num_vertices, left, right)
-    # print(paths)
-    # print(counted_vertices)
-    assert(counted_vertices == num_vertices)
-
-    return char_num, paths
-
-
 
 def parse_hershey_file(f):
 
     ctoi = lambda a: ord(a) - ord('R')
-    get_coord = lambda a: (tuple(map(ctoi, a[i:i+2])) for i in range(0, len(a), 2))
 
     def parse_hershey_char_info(code):
-        char_num = int(code[1:5].strip())
-        num_vertices = int(code[5:8].strip())
-        left = ctoi(code[8])
-        right = ctoi(code[9])
-        return char_num, num_vertices-1
+        char_num = int(code[0:4].strip())
+        num_vertices = int(code[4:7].strip())
+        left = ctoi(code[7])
+        right = ctoi(code[8])
+        return char_num, num_vertices-1  # return the number of vertices - left/right pair
 
-    def parse_hershey_char_paths(num_vertices, code):
-        path_strs = code.replace('\n', '').split(' R')
-        paths = tuple(tuple(get_coord(p)) for p in path_strs)
+    def parse_hershey_char_paths(num_vertices, f):
+        paths = []
+        cur_path = []
+        counted_vertices = 0
+        while(counted_vertices < num_vertices):
+            x = f.read(1)
+            if x == '\n':
+                continue
+            y = f.read(1)
+            if x == ' ' and y == 'R':
+                paths.append(cur_path)
+                cur_path = []
+            else:
+                cur_path.append((ctoi(x), -ctoi(y)))  # flip y values
+            counted_vertices += 1
+        paths.append(cur_path)
         return paths
 
     while(True):
-        info_line = f.read(10)
-        if info_line == '':
+        while (f.read(1) == '\n'):  # skip newlines and absorb leading space of new character
+            pass
+        info_line = f.read(9)
+        if info_line == '':  # exit on EOF
             break
-        print(info_line)
         char_num, num_vertices = parse_hershey_char_info(info_line)
-        num_chars = num_vertices*2 + ((num_vertices*2+10) // 72) + 1
-        paths_str = f.read(num_chars)
-        paths = parse_hershey_char_paths(paths_str)
-        counted_vertices = (len(paths)-1) + sum((len(p) for p in paths))
-        assert counted_vertices == num_vertices, "{}: {!r}".format(char_num, paths_str)
+        paths = parse_hershey_char_paths(num_vertices, f)
         yield char_num, paths
 
-
-
-    # f.read()
-    # text = f.read()
-    # return {c: p for c, p in map(parse_hershey_char, text[1:].split('\n '))}
 
 def make_ascii_charcode_map(hmap: str):
     paths = hmap.split()
@@ -89,13 +62,20 @@ def make_ascii_charcode_map(hmap: str):
             i += 1
     return charcode_to_ord
 
+
 def plot_charpath(charpath):
     for p in charpath:
         x, y = zip(*p)
         plt.plot(x, y)
+    plt.axis('equal')
 
 
-# def make_gcode(characters)
+def calc_dimensions(charpath):
+    xs, ys = zip(*sum(charpath, []))
+    width = max(xs) - min(xs)
+    height = max(ys) - min(ys)
+    return width, height
+
 
 if __name__ == "__main__":
     parse_hershey_char("    8  9MWOMOV RUMUV ROQUQ\n")
